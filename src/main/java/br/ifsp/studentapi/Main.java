70941d474b580b46c0f8a7a26842a61774eb449b
Matheus <matheusg.farias@hotmail.com>
2026-02-24 11:49:07 -0300

    feat: inital commit

---
+++ src/main/java/br/ifsp/studentapi/Main.java
@@ -0,0 +1,36 @@
+package br.ifsp.studentapi;
+
+import br.ifsp.studentapi.model.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.math.BigDecimal;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.setName("Matheus");
+        student.setRa("SC304887X");
+        student.setEmail("matheus@ifsp.edu.br");
+        student.setGrade1(BigDecimal.valueOf(10));
+        student.setGrade2(BigDecimal.valueOf(9));
+        student.setGrade3(BigDecimal.valueOf(8));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("studentapi");
+
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(student);
+
+        em.getTransaction().commit();
+
+        em.close();
+
+    }
+}
