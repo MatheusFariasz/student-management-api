00e9253d20579a59687043d0ba51b7dbdb86a5ed
Matheus <matheusg.farias@hotmail.com>
2026-02-25 18:21:24 -0300

    feat: implement student CRUD operations

--- src/main/java/br/ifsp/studentapi/dao/StudentDAO.java
+++ src/main/java/br/ifsp/studentapi/dao/StudentDAO.java
@@ -2,6 +2,9 @@ package br.ifsp.studentapi.dao;
 
 import br.ifsp.studentapi.model.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+
+import java.util.List;
 
 public class StudentDAO {
     private final EntityManager em;
@@ -12,5 +15,33 @@ public class StudentDAO {
 
     public void create(Student student){
         em.persist(student);
+    }
+
+    public void delete(String nome){
+        Student student = em.find(Student.class, nome);
+        if(student != null){
+            em.remove(student);
+        }
+    }
+
+    public void update(Student student){
+        em.merge(student);
+    }
+
+    public Student findByName(String nome){
+        String jpql = "SELECT s FROM Student s WHERE s.name = :n";
+        try {
+            return em.createQuery(jpql, Student.class)
+                    .setParameter("n", nome)
+                    .setMaxResults(1)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    public List<Student> findAll(){
+        String jpql = "select s from Student s ";
+        return em.createQuery(jpql, Student.class).getResultList();
     }
 }
